79beafbdb478bd7612a31c54f0a46863269add8b
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-09-10 22:45:07 -0300

    feat(wip): healtcheck endpoints

--- src/EloVet.Api/Program.cs
+++ src/EloVet.Api/Program.cs
@@ -1,4 +1,7 @@
 using EloVet.Infrastructure.Mongo;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 
 //Configuração do MongoDB
 builder.Services.AddMongoDb(builder.Configuration);
+
+//Health Check
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy(), tags: new[] { "live" })
+    .AddCheck<MongoDbHealthCheck>("mongodb", tags: new[] { "ready" });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,5 +31,20 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Configura o endpoint de Liveness (Vivacidade)
+// Retorna 200 OK imediatamente se a aplicação não estiver travada.
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = check => check.Tags.Contains("live")
+}).WithOpenApi();
+
+// Configura o endpoint de Readiness (Prontidão)
+// Só retorna 200 OK se todas as dependências com a tag "ready" (como o banco de dados) estiverem ok.
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+    Predicate = check => check.Tags.Contains("ready"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse // Formata a saída em um JSON amigável
+}).WithOpenApi();
 
 app.Run();
