7f2f56ee05bc33e658f1929d7693553860ec7e17
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-09-11 00:49:14 -0300

    fix(wip): register IMongoDatabase in dependency injection

--- src/EloVet.Infrastructure/Mongo/MongoDependencyInjection.cs
+++ src/EloVet.Infrastructure/Mongo/MongoDependencyInjection.cs
@@ -9,14 +9,21 @@ public static class MongoDependencyInjection
     public static IServiceCollection AddMongoDb (this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetSection("MongoDb:ConnectionString").Value ?? throw new InvalidOperationException("Connection String não configurada.");
-        
-        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+        services.AddSingleton<IMongoClient>(
+            new MongoClient(connectionString));
+
+        services.AddSingleton<IMongoDatabase>(serviceProvider =>
+        {
+            var mongoClient = serviceProvider.GetRequiredService<IMongoClient>();
+            return mongoClient.GetDatabase("EloVet");
+        });
 
         services.AddHealthChecks()
             .AddCheck<MongoDbHealthCheck>(
                 "mongodb",
                 tags: new[] { "ready" });
-                
+
         return services;
     }
 }
