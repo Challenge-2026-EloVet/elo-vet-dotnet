674f88825666f8888f8655b0fa3313e99ec183fb
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-09-11 00:57:34 -0300

    feat(wip): adding controller annotations

--- src/EloVet.Api/Controllers/ProntuarioController.cs
+++ src/EloVet.Api/Controllers/ProntuarioController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EloVet.Controllers.ProntuarioController;
 
+[ApiController]
+[Route("api/[controller]")]
 public class ProntuarioController : ControllerBase
 {
     private readonly IProntuarioService _prontuarioService;
